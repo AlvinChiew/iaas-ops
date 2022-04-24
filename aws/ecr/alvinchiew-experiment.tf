492d528639411da98b31225b27a5945d34cbb31b
Alvin Chiew <jingjie95@gmail.com>
2022-04-25 00:56:31 +0800

    remove terraform outputs

--- aws/ecr/alvinchiew-experiment.tf
+++ aws/ecr/alvinchiew-experiment.tf
@@ -1,5 +1,5 @@
-module "experiment" {
-  source          = "./module" # "github.com/naluri-hidup/tf-ecr-module.git?ref=v1.0.1"
+module "experiment_ecr" {
+  source          = "./module" # "github.com/alvinchiew/tf-ecr-module.git?ref=v1.0.1"
   repository_name = "alvinchiew-experiment"
   service_name    = "alvinchiew-experiment-service"
   team_name       = "infra"
