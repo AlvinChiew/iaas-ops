22c7a010306b95f3e2c998fa0fd7d0fec9c4e9ec
Alvin Chiew <jingjie95@gmail.com>
2022-04-25 15:18:59 +0800

    add subnets to prod and stag vpc

--- aws/vpc/main-prod.tf
+++ aws/vpc/main-prod.tf
@@ -12,3 +12,33 @@ resource "aws_vpc" "prod_alvinchiew" {
     prevent_destroy = true
   }
 }
+
+resource "aws_subnet" "subnet_prod_alvinchiew_a" {
+  vpc_id            = aws_vpc.prod_alvinchiew.id
+  cidr_block        = "20.1.0.0/20"
+  availability_zone = "ap-southeast-1a"
+
+  tags = {
+    Name = "subnet-prod-alvinchiew-a"
+  }
+}
+
+resource "aws_subnet" "subnet_prod_alvinchiew_b" {
+  vpc_id            = aws_vpc.prod_alvinchiew.id
+  cidr_block        = "20.1.16.0/20"
+  availability_zone = "ap-southeast-1b"
+
+  tags = {
+    Name = "subnet-prod-alvinchiew-b"
+  }
+}
+
+resource "aws_subnet" "subnet_prod_alvinchiew_c" {
+  vpc_id            = aws_vpc.prod_alvinchiew.id
+  cidr_block        = "20.1.32.0/20"
+  availability_zone = "ap-southeast-1c"
+
+  tags = {
+    Name = "subnet-prod-alvinchiew-c"
+  }
+}
--- aws/vpc/main-stag.tf
+++ aws/vpc/main-stag.tf
@@ -13,3 +13,33 @@ resource "aws_vpc" "stag_alvinchiew" {
     prevent_destroy = true
   }
 }
+
+resource "aws_subnet" "subnet_stag_alvinchiew_a" {
+  vpc_id            = aws_vpc.stag_alvinchiew.id
+  cidr_block        = "30.1.0.0/20"
+  availability_zone = "ap-southeast-1a"
+
+  tags = {
+    Name = "subnet-stag-alvinchiew-a"
+  }
+}
+
+resource "aws_subnet" "subnet_stag_alvinchiew_b" {
+  vpc_id            = aws_vpc.stag_alvinchiew.id
+  cidr_block        = "30.1.16.0/20"
+  availability_zone = "ap-southeast-1b"
+
+  tags = {
+    Name = "subnet-stag-alvinchiew-b"
+  }
+}
+
+resource "aws_subnet" "subnet_stag_alvinchiew_c" {
+  vpc_id            = aws_vpc.stag_alvinchiew.id
+  cidr_block        = "30.1.32.0/20"
+  availability_zone = "ap-southeast-1c"
+
+  tags = {
+    Name = "subnet-stag-alvinchiew-c"
+  }
+}
