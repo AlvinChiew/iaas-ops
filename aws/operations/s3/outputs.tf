f5536901b0e4a1bb2d6be82e44e4441bd0a9ead4
Alvin Chiew <jingjie95@gmail.com>
2022-05-02 23:02:10 +0800

    create output for s3 bucket webpage url

---
+++ aws/operations/s3/outputs.tf
@@ -0,0 +1,3 @@
+output "s3_unika_url" {
+  value = aws_s3_bucket_website_configuration.webapge_unika.website_endpoint
+}
--- aws/outputs.tf
+++ aws/outputs.tf
@@ -5,3 +5,7 @@
 # output "stag_vpc_id" {
 #   value = module.vpc.stag_vpc_id
 # }
+
+# output "s3_unika_url" {
+#   value = module.s3.s3_unika_url
+# }
